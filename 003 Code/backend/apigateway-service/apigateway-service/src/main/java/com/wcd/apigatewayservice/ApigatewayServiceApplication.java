612d4ef21d5831babac744132c69194eec5842ff
Sanizzang <tksl4732@naver.com>
2023-04-09 22:40:40 +0900

    feat: apigateway-service 추가

---
+++ 003 Code/backend/apigateway-service/apigateway-service/src/main/java/com/wcd/apigatewayservice/ApigatewayServiceApplication.java
@@ -0,0 +1,14 @@
+package com.wcd.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApigatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayServiceApplication.class, args);
+	}
+
+}
