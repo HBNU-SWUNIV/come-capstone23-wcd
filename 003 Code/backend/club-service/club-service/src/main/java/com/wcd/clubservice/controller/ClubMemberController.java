6130e9ab33cf868eb15a2fa2ee7eeae088a780e1
Sanizzang <tksl4732@naver.com>
2023-06-07 10:57:30 +0900

    feat: club-service Swagger 적용

--- 003 Code/backend/club-service/club-service/src/main/java/com/wcd/clubservice/controller/ClubMemberController.java
+++ 003 Code/backend/club-service/club-service/src/main/java/com/wcd/clubservice/controller/ClubMemberController.java
@@ -3,37 +3,41 @@ package com.wcd.clubservice.controller;
 import com.wcd.clubservice.dto.clubMember.response.ResponseClubMembersByClubId;
 import com.wcd.clubservice.dto.clubMember.request.RequestJoinClubMember;
 import com.wcd.clubservice.service.member.ClubMemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "모임 멤버 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/")
 public class ClubMemberController {
     private final ClubMemberService clubMemberService;
 
-    // 모임 가입
+    @Operation(summary = "모임 가입", description = "모임 가입")
     @PostMapping("/member")
     public ResponseEntity<Long> createClubMember(@Valid @RequestBody RequestJoinClubMember requestJoinClubMember) {
         return ResponseEntity.status(HttpStatus.CREATED).body(clubMemberService.createClubMember(requestJoinClubMember));
     }
 
-    // 모임 멤버 조회
+    @Operation(summary = "모임 멤버 조회", description = "club-id에 해당하는 모임 멤버 조회")
     @GetMapping("/member/club/{club-id}")
     public ResponseEntity<ResponseClubMembersByClubId> getClubMembers(@PathVariable("club-id") Long clubId) {
         return ResponseEntity.status(HttpStatus.OK).body(clubMemberService.getClubMembers(clubId));
     }
 
-    // 모임 탈퇴
+    @Operation(summary = "모임 멤버 삭제", description = "club-id, user-id에 해당하는 모임 멤버 삭제")
     @DeleteMapping("/member/{user-id}/club/{club-id}")
     public ResponseEntity<Void> deleteClubMember(@PathVariable("user-id") Long userId, @PathVariable("club-id") Long clubId) {
         clubMemberService.deleteClubMember(userId, clubId);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "모임 멤버 삭제", description = "user-id에 해당하는 모임 멤버 삭제")
     @DeleteMapping("/member/{user-id}")
     public ResponseEntity<Void> deleteMember(@PathVariable("user-id") Long userId) {
         clubMemberService.deleteMember(userId);
--- 003 Code/backend/club-service/club-service/src/main/java/com/wcd/clubservice/dto/club/response/ResponseClub.java
+++ 003 Code/backend/club-service/club-service/src/main/java/com/wcd/clubservice/dto/club/response/ResponseClub.java
@@ -3,24 +3,45 @@ package com.wcd.clubservice.dto.club.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.querydsl.core.annotations.QueryProjection;
 import com.wcd.clubservice.enums.ApprovalMethod;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Schema(description = "모임 정보 응답")
 @Getter
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseClub {
+    @Schema(description = "모임 번호")
     private Long id;
+
+    @Schema(description = "모임장 유저번호")
     private Long hostId;
+
+    @Schema(description = "모임 이름")
     private String clubName;
+
+    @Schema(description = "카테고리")
     private String category;
+
+    @Schema(description = "모임 설명")
     private String description;
+
+    @Schema(description = "모임 메인 이미지")
     private String mainImageUrl;
+
+    @Schema(description = "승인 방법")
     private ApprovalMethod approvalMethod;
+
+    @Schema(description = "최대 수용 인원")
     private int maximumPeople;
+
+    @Schema(description = "모집 여부")
     private boolean recruitment;
+
+    @Schema(description = "생성 날짜")
     private LocalDateTime createdAt;
 
     @QueryProjection
