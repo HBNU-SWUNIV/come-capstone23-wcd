def79ea6ed05097d0cd80ddae0031012c548c5fc
Sanizzang <tksl4732@naver.com>
2023-06-07 10:57:02 +0900

    feat: user-service Swagger 적용

--- 003 Code/backend/user-service/user-service/src/main/java/com/wcd/userservice/client/ClubServiceClient.java
+++ 003 Code/backend/user-service/user-service/src/main/java/com/wcd/userservice/client/ClubServiceClient.java
@@ -1,18 +1,12 @@
 package com.wcd.userservice.client;
 
-import com.wcd.userservice.dto.club.ResponseClub;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
 
 @FeignClient(name="club-service")
 public interface ClubServiceClient {
-
-    @GetMapping("/club-service/clubs/member/{user-id}")
-    List<ResponseClub> getClubByUserId(@PathVariable Long userId);
 
     @DeleteMapping("/club-service/member/{user-id}")
     void deleteMember(@PathVariable("user-id") Long userId);
