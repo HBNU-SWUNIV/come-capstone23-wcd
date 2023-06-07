6fff278b46a1dec9ceb31bf243a84f7dad0af5e9
Sanizzang <tksl4732@naver.com>
2023-06-07 10:57:55 +0900

    feat: chatting-service Swagger 적용

--- 003 Code/backend/chatting-service/chatting-service/src/main/java/com/wcd/chattingservice/dto/response/ResponseChat.java
+++ 003 Code/backend/chatting-service/chatting-service/src/main/java/com/wcd/chattingservice/dto/response/ResponseChat.java
@@ -1,18 +1,29 @@
 package com.wcd.chattingservice.dto.response;
 
 import com.querydsl.core.annotations.QueryProjection;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Schema(description = "채팅 정보 응답")
 @Getter
 @NoArgsConstructor
 public class ResponseChat {
+    @Schema(description = "채팅 번호")
     private Long id;
+
+    @Schema(description = "채팅방 번호")
     private Long chatRoomId; // 방 번호
+
+    @Schema(description = "채팅 송신자 멤버번호")
     private Long senderId;
+
+    @Schema(description = "메세지")
     private String message;
+
+    @Schema(description = "채팅 보낸 시간")
     private LocalDateTime sendTime;
 
     @QueryProjection
