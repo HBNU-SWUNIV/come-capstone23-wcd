133024a253c4a4d531c598475b6982f6d980caf8
seongjun-noh <seongjun5657@gmail.com>
2023-08-01 17:23:33 +0900

    refactor: apigateway-service AuthorizationHeaderFilter.java 필요없는 코드 삭제

--- 003 Code/backend/apigateway-service/apigateway-service/src/main/java/com/wcd/apigatewayservice/filter/AuthorizationHeaderFilter.java
+++ 003 Code/backend/apigateway-service/apigateway-service/src/main/java/com/wcd/apigatewayservice/filter/AuthorizationHeaderFilter.java
@@ -37,43 +37,6 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
 
     public static class Config { }
 
-//    @Override
-//    public GatewayFilter apply(Config config) {
-//        return (((exchange, chain) -> {
-//            ServerHttpRequest request = exchange.getRequest();
-//
-//            // HTTP 요청의 Authorization 헤더가 있는지 확인
-//            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-//                return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
-//            }
-//            // HTTP 요청 헤더에서 Authorization 필드에 해당하는 값을 가져옴
-//            String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);;
-//            // authorizationHeader 문자열에 Bearer 문자열을 제거
-//            String jwt = authorizationHeader.replace("Bearer ", "");
-//
-//            // JWT 토큰의 서명을 검증하고, 만료 시간을 확인하여 JWT 토큰이 유효한지 검증
-//            if (jwt != null && isJwtValid(jwt)) {
-//                log.info(jwt);
-//                // Redis에 해당 accessToken logout 여부를 확인
-//                String isLogout = (String) redisTemplate.opsForValue().get(jwt);
-//
-//                // 로그아웃이 없는(되어 있지 않은) 경우 해당 토큰은 정상적으로 작동하기
-//                if (!ObjectUtils.isEmpty(isLogout)) {
-//
-//                    return onError(exchange, "Please Login", HttpStatus.UNAUTHORIZED);
-//                }
-//
-//            }
-//            else {
-//                log.info("JWT token is not valid");
-//                return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
-//            }
-//
-//            return chain.filter(exchange);
-//
-//        }));
-//    }
-
     @Override
     public GatewayFilter apply(Config config) {
         return (((exchange, chain) -> {
