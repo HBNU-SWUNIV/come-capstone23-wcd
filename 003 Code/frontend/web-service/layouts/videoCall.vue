7a2cedd073a2ae71d424d936b4e4e04d2c4f0610
seongjun-noh <seongjun5657@gmail.com>
2023-10-08 14:58:38 +0900

    no message

---
+++ 003 Code/frontend/web-service/layouts/videoCall.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app class="login-page">
+    <div class="centered-content" style="height:100%;">
+      <Nuxt />
+    </div>
+  </v-app>
+</template>
+
+<style scoped>
+
+</style>
--- 003 Code/frontend/web-service/pages/videoCall.vue
+++ 003 Code/frontend/web-service/pages/videoCall.vue
@@ -76,7 +76,7 @@ export default {
     UserVideo,
   },
   layout(context) {
-    return "login";
+    return "videoCall";
   },
 
   data() {
