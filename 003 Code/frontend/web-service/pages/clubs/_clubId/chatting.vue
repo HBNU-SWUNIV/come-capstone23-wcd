e86d10bf3955a6b20d75bd3b99e41c440ecdbe9c
JeonJaeYun <nick9297@naver.com>
2023-10-17 20:11:53 +0900

    채팅구현

--- 003 Code/frontend/web-service/pages/clubs/_clubId/chatting.vue
+++ 003 Code/frontend/web-service/pages/clubs/_clubId/chatting.vue
@@ -12,7 +12,16 @@
     >
       <h3>채팅방</h3>
     </div>
-    <div style="height: 600px"></div>
+    <div style="height: 600px">
+      <ul>
+        <li v-for="testmessage in testMessages" :key="testmessage.id" style="color:black;">
+          {{ testmessage.senderId }}: {{ testmessage.message }} [{{ testmessage.sendTime }}]
+        </li>
+        <li v-for="message in messages" :key="message.id" style="color:black;">
+          {{ message.sender }}: {{ message.message }} [{{ message.sendTime }}]
+        </li>
+      </ul>
+    </div>
     <div style="height: 50px; background-color: gray"></div>
     <div style="height: 110px; padding: 5px">
       <textarea
@@ -33,20 +42,89 @@
 </template>
 
 <script>
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
+
 export default {
   data() {
     return {
       clubId: null,
+      message: "",
+      messages: [],
+      stompClient: null,
+      testMessages: [],
     };
   },
   layout(context) {
     return "chatting";
   },
   methods: {
-   
+    connect() {
+      const socket = new SockJS("http://211.115.222.246:8777/ws");
+      this.stompClient = Stomp.over(socket);
+      this.stompClient.connect({}, this.onConnected, this.onError);
+    },
+    onConnected(){
+      this.stompClient.subscribe(`/topic/${this.clubId}`, this.onMessageReceived)
+    },
+    onError(error){
+      console.error(error)
+    },
+    onMessageReceived(payload){
+      const parseMessage = JSON.parse(payload.body);
+      this.messages.push(parseMessage);
+    },
+    sendMessage() {
+      if (!this.message.trim()) {
+        return;
+      }
+
+      let chat = {
+        clubId: this.clubId,
+        senderId: sessionStorage.getItem("user_id"),
+        message: this.message,
+      };
+
+      if (this.stompClient) {
+        this.stompClient.send("/app/chat/send", {}, JSON.stringify(chat));
+        this.message = "";
+      } else {
+        console.error("WebSocket connection is not established.");
+      }
+    },
+    async getMessage(){
+      try {
+        const access_token = this.$store.state.access_token;
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${access_token}`,
+          },
+          params: {
+            clubId: this.clubId,
+            page: 0,
+            size: 10,
+          },
+        };
+        await this.$axios
+          .get(
+            `http://211.115.222.246:8777/chat`,
+            config
+          )
+          .then((res) => {
+            console.log(res.data.content);
+            this.testMessages = res.data.content.reverse();
+            console.log(this.testMessages)
+          });
+      } catch (err) {
+        console.error("err", err);
+      }
+    }
   },
   created() {
     this.clubId = this.$route.params.clubId;
+    this.connect();
+    this.getMessage()
   },
 };
 </script>
