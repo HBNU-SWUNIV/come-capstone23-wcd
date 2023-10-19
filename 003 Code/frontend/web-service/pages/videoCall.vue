1365afc07555d0c38b51f655c69aa13f46af55b5
seongjun-noh <seongjun5657@gmail.com>
2023-10-19 15:32:12 +0900

    videoCall 구현

--- 003 Code/frontend/web-service/pages/videoCall.vue
+++ 003 Code/frontend/web-service/pages/videoCall.vue
@@ -1,7 +1,7 @@
-<template>
-  <div id="main" style="text-align: center;">
-    <h1>Join a video session</h1>
-    <form @submit.prevent="joinSession">
+<template style="display: flex;
+            justify-content: center;height: 100%; width: 100%;">
+  <div id="main" style="text-align: center; width: 100%">
+    <form @submit.prevent="joinSession" v-if="showForm">
       <p>
         <label>Session:</label>
         <input type="text" v-model="sessionName" required>
@@ -15,31 +15,61 @@
       </p>
     </form>
 
-    <openvidu-webcomponent ref="webComponent" style="height: 100%;"></openvidu-webcomponent>
+    <openvidu-webcomponent ref="webComponent" style="height: 100%; width: 100%;"></openvidu-webcomponent>
   </div>
 </template>
 
-<script src="../static/openvidu-webcomponent-2.28.0.js"></script>
 <script>
 import $ from 'jquery'; // Assuming you're using jQuery
 
 export default {
+  head() {
+    return {
+      script: [
+        {src: '/openvidu-webcomponent-2.28.0.js'}
+      ],
+      link: [
+        { rel: 'stylesheet', href: '/openvidu-webcomponent-2.28.0.css' }
+      ]
+    }
+  },
   data() {
     return {
       sessionName: 'SessionA',
       participantName: 'User1',
       APPLICATION_SERVER_URL: 'https://wcd.kro.kr/api/openvidu/',
+      showForm: true,
+      webComponent: null,
     };
   },
   mounted() {
-    const webComponent = this.$refs.webComponent;
+    this.webComponent = this.$refs.webComponent;
 
-    webComponent.addEventListener('onSessionCreated', (event) => {
-      // ... your code ...
+    this.webComponent.addEventListener('onSessionCreated', (event) => {
+      const session = event.detail;
+
+      session.on('connectionCreated', (e) => {
+        console.log("connectionCreated", e);
+      });
+
+      session.on('streamDestroyed', (e) => {
+        console.log("streamDestroyed", e);
+      });
+
+      session.on('streamCreated', (e) => {
+        console.log("streamCreated", e);
+      });
+
+      session.on('sessionDisconnected', (event) => {
+        console.warn("sessionDisconnected event");
+        this.showForm = true;
+        this.webComponent.style.display = 'none';  // This line still directly manipulates the DOM
+      });
+
+      session.on('exception', (exception) => {
+        console.error(exception);
+      });
     });
-    // ... other event listeners ...
-
-    this.hideForm();
   },
   methods: {
     async joinSession() {
@@ -51,9 +81,9 @@ export default {
         webcam: tokens[0],
         screen: tokens[1]
       };
-    },
-    hideForm() {
-      this.$refs.main.style.display = 'none';
+      console.log("dasda");
+      this.showForm = false;
+      this.webComponent.style.display = 'block';
     },
     async getToken(mySessionId) {
       const sessionId = await this.createSession(mySessionId);
@@ -86,5 +116,3 @@ export default {
   }
 }
 </script>
-
-<style src="../static/openvidu-webcomponent-2.28.0.css"></style>
