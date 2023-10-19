3381264126bdc5f345410e4b9e59db1af4a8ccec
seongjun-noh <seongjun5657@gmail.com>
2023-10-19 10:38:18 +0900

    no message

--- 003 Code/frontend/web-service/nuxt.config.js
+++ 003 Code/frontend/web-service/nuxt.config.js
@@ -112,9 +112,9 @@ export default {
   devServer: {
     disableHostCheck: true,
     https: {
-      key: path.join(__dirname, process.env.SSL_PRIVATE_KEY),
-      cert: path.join(__dirname, process.env.SSL_CERTIFICATE),
-      ca: path.join(__dirname, process.env.SSL_CA_BUNDLE),
+      key: fs.readFileSync(path.resolve(__dirname, process.env.SSL_PRIVATE_KEY)),
+      cert: fs.readFileSync(path.resolve(__dirname, process.env.SSL_CERTIFICATE)),
+      ca: fs.readFileSync(path.resolve(__dirname, process.env.SSL_CA_BUNDLE)),
       secureProtocol: 'TLSv1_2_method'  // Use TLSv1.2
     }
   },
