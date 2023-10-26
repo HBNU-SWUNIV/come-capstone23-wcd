3337223d693e1476ad56d84f02ec3a3df294565f
Sanizzang <tksl4732@naver.com>
2023-10-26 20:24:35 +0900

    feat: cors 설정 삭제

--- 003 Code/backend/alarm-service/src/main/java/com/wcd/alarmservice/config/WebConfig.java
+++ 003 Code/backend/alarm-service/src/main/java/com/wcd/alarmservice/config/WebConfig.java
@@ -1,19 +1,19 @@
-package com.wcd.alarmservice.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
-
-    @Override
-    public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://wcd.kro.kr", "http://wcd.kro.kr", "http://localhost:3000", "https://localhost:3000", "http://localhost:8080", "10.1.1.66")
-                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
-                .exposedHeaders(HttpHeaders.LOCATION);
-    }
-}
+//package com.wcd.alarmservice.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+//
+//    @Override
+//    public void addCorsMappings(final CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("https://wcd.kro.kr", "http://wcd.kro.kr", "http://localhost:3000", "https://localhost:3000", "http://localhost:8080", "10.1.1.66")
+//                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+//                .exposedHeaders(HttpHeaders.LOCATION);
+//    }
+//}
--- 003 Code/backend/fcm-service/src/main/java/com/wcd/fcmservice/fcmservice/config/CorsConfig.java
+++ 003 Code/backend/fcm-service/src/main/java/com/wcd/fcmservice/fcmservice/config/CorsConfig.java
@@ -1,19 +1,19 @@
-package com.wcd.fcmservice.fcmservice.config;
-
-import com.google.common.net.HttpHeaders;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
-
-    @Override
-    public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://wcd.kro.kr", "http://wcd.kro.kr", "http://localhost:3000", "https://localhost:3000", "http://localhost:8080", "10.1.1.66")
-                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
-                .exposedHeaders(HttpHeaders.LOCATION);
-    }
-}
+//package com.wcd.fcmservice.fcmservice.config;
+//
+//import com.google.common.net.HttpHeaders;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+//
+//    @Override
+//    public void addCorsMappings(final CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("https://wcd.kro.kr", "http://wcd.kro.kr", "http://localhost:3000", "https://localhost:3000", "http://localhost:8080", "10.1.1.66")
+//                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+//                .exposedHeaders(HttpHeaders.LOCATION);
+//    }
+//}
