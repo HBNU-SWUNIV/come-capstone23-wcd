274c82aa98426e070b6053bf6574a86f042338db
Sanizzang <tksl4732@naver.com>
2023-10-27 16:37:18 +0900

    feat: apigateway-service cors 헤더 제거 필터

---
+++ 003 Code/backend/apigateway-service/src/main/java/com/wcd/apigatewayservice/filter/CorsRemoveFilter.java
@@ -0,0 +1,37 @@
+package com.wcd.apigatewayservice.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CorsRemoveFilter extends AbstractGatewayFilterFactory<CorsRemoveFilter.Config> {
+
+    public CorsRemoveFilter() {
+        super(Config.class);
+    }
+
+    public static class Config { }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            // ServerHttpRequest 및 ServerHttpResponse 객체 가져오기
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            // HttpHeaders 가져오기
+            HttpHeaders headers = response.getHeaders();
+
+            // 특정 헤더 삭제
+            headers.remove(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN);
+            headers.remove(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS);
+
+            // 수정된 헤더로 응답 보내기
+            return chain.filter(exchange.mutate().response(response).build());
+        };
+    }
+}
