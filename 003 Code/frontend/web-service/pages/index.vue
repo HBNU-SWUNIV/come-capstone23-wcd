977020ca47b26e3b7e826c847784ebbc53ddddb9
seongjun-noh <seongjun5657@gmail.com>
2023-11-27 23:55:16 +0900

    no message

--- 003 Code/frontend/web-service/pages/index.vue
+++ 003 Code/frontend/web-service/pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div style="display: flex; flex-direction: column; justify-content: space-between; width: 100%; padding: 30px; gap: 20px">
     <ClubCategoryCard style="height: 150px"/>
-    <ClubRecommendCard style="flex-grow: 2"/>
-    <div style="display: flex; flex-direction: row; flex-grow: 2; gap: 20px">
+    <ClubRecommendCard style="height: 50%"/>
+    <div style="display: flex; flex-direction: row; height: 50%; gap: 20px">
       <MyClubCard />
       <MyCreateClubCard />
     </div>
